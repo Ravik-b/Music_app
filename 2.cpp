486e537369a1472fdccb389908b10ba11563bd6b
RAVI <ravikaushik09042000@gmail.com>
2023-02-18 18:14:03 +0530

    Initial commit

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+                   
+using namespace std;
+                    
+int main()
+{
+      
+  
+             for (int i =48; i <= 122; i++)
+        { cout<<char(i)<< endl;       
+
+           
+        }
+        
+                
+    return 0; 
+ }
